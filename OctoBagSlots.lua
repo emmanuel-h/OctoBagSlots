89cc410a659717e4d39bbd4abdb5f14a3e38ba9d
emmanuel-h <tchezmoi@gmail.com>
2019-09-18 18:24:50 +0200

    Set up the display

--- OctoBagSlots.lua
+++ OctoBagSlots.lua
@@ -1,8 +1,32 @@
---
--- Created by IntelliJ IDEA.
--- User: Microsoft Windows
--- Date: 18-Sep-19
--- Time: 16:50
--- To change this template use File | Settings | File Templates.
---
+local function UpdateBagSlots(self, elapsed)
+    print("changement")
+    OctoBagSlotsFontString:SetText("55")
+end
 
+function OnLoad(self, event, ...)
+    self:RegisterEvent("ADDON_LOADED")
+end
+
+function BagChange(self, event, ...)
+    if (event == "ADDON_LOADED" and ... == "OctoBagSlots") then
+        print("OctoBagSlots loaded")
+        self:UnregisterEvent("ADDON_LOADED")
+        self:RegisterEvent("BAG_UPDATE")
+        OctoBagSlots:SetSize(20, 20)
+        OctoBagSlots:SetPoint("BOTTOMRIGHT", -267, 2)
+        OctoBagSlots:SetScript("OnEvent", UpdateBagSlots)
+        local coordsFont2 = OctoBagSlots:CreateFontString("OctoBagSlotsFontString", "ARTWORK", "GameTooltipText")
+        OctoBagSlots:SetBackdrop({
+            bgFile = [[Interface\DialogFrame\UI-DialogBox-Background]],
+            tile = true, tileSize = 8, edgeSize = 8,
+            insets = { left = 1, right = 1, top = 1, bottom = 3 }
+        })
+        local slotsEmpty = 55
+        OctoBagSlotsFontString:SetText(slotsEmpty)
+        OctoBagSlotsFontString:SetTextColor(87, 213, 59, 1)
+        OctoBagSlots:SetFrameLevel(10)
+        coordsFont2:SetPoint("CENTER", "OctoBagSlots", "CENTER", 0, 0)
+        coordsFont2:Show()
+        OctoBagSlots:Show()
+    end
+end
